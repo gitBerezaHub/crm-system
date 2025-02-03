def4184ee9e053a7bb0bbfec3805917e92f40b23
gitBerezaHub <berezinkb06@gmail.com>
2025-02-03 11:38:14 +0700

    settings for tailwind, add icons

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -9,6 +9,10 @@
     </NuxtLink>
 
     <UiButton>Send</UiButton>
+
+    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+      <Icon name="line-md:logout" size="20"/>
+    </button>
   </aside>
 </template>
 
